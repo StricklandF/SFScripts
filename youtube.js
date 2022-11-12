9951e5fed26c987468fa61e6baeaa1de6b06ea5b
StricklandF <83151298+StricklandF@users.noreply.github.com>
2022-11-12 21:45:35 +0800

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -86,7 +86,7 @@ if (needProcessFlag) {
 function mapItems(itemList, isBanner = false) {
   itemList?.forEach?.((ie) => {
     if (isBanner) {
-      removeBanner(ie?.g2F117866661);
+      // removeBanner(ie?.g2F117866661);
     } else {
       removeAd(ie?.n4F50195462);
       removeAd(ie?.n4F51845067?.n5F5?.n6F51431404);
