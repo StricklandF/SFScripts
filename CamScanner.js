67ac45370b02fadf8fcad9a57ee59f420dd9f59e
StricklandF <83151298+StricklandF@users.noreply.github.com>
2022-11-12 17:12:17 +0800

    Create CamScanner.js

---
+++ CamScanner.js
@@ -0,0 +1,46 @@
+/*
+CamScanner 解锁部分高级特权
+
+***************************
+Quantumult X:
+
+[rewrite_local]
+^https:\/\/(api|api-cs|api-cs-cn|api-center)\.intsig\.net\/purchase\/cs\/query_property\? url script-response-body https://raw.githubusercontent.com/StricklandF/SFScripts/main/camscanner.js
+
+[mitm]
+hostname = ap*.intsig.net
+
+***************************
+Surge4 or Loon:
+
+[Script]
+http-response https:\/\/(api|api-cs|api-cs-cn|api-center)\.intsig\.net\/purchase\/cs\/query_property\? requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/StricklandF/SFScripts/main/camscanner.js
+
+[MITM]
+hostname = ap*.intsig.net
+
+**************************/
+
+let body = JSON.parse($response.body)
+    body.data.psnl_vip_property = {"renew_method": "appstore",
+      "initial_tm": "1614867690",
+      "svip": 1,
+      "auto_renewal": true,
+      "ms_first_pay": 0,
+      "pending": 0,
+      "group2_paid": 0,
+      "inherited_flag": 0,
+      "nxt_renew_tm": "9915126887",
+      "level_info": {
+        "level": 1,
+        "days": 1,
+        "end_days": 30
+      },
+      "group1_paid": 1,
+      "ys_first_pay": 0,
+      "renew_type": "year",
+      "expiry": 8487890487,
+      "grade": 2,
+      "last_payment_method": "appstore",
+      "product_id": "com.intsig.camscanner.premiums.oneyear.autorenewable.svip.low"}
+    $done({body:JSON.stringify(body)})
