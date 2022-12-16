261253463ed91594a3e09803b8b455e24f08db97
StricklandF <83151298+StricklandF@users.noreply.github.com>
2022-12-16 21:58:31 +0800

    Update RedditAds.js

--- RedditAds.js
+++ RedditAds.js
@@ -1,13 +1,7 @@
-if ($response.body.includes("adEvents" | "Promoted")) {
+if ($response.body.includes("Promoted")) {
   let body = JSON.parse($response.body);
   if (body.data.subredditInfoByName) {
-    body.data.subredditInfoByName.elements.edges = body.data.subredditInfoByName.elements.edges.filter((n) => !!!n.node.adEvents);
-    $done({ body: JSON.stringify(body) });
-  } else if (body.data.subredditInfoByName) {
     body.data.subredditInfoByName.elements.edges = body.data.subredditInfoByName.elements.edges.filter((n) => !!!n.node.Promoted);
-    $done({ body: JSON.stringify(body) });
-  } else if (body.data.home) {
-    body.data.home.elements.edges = body.data.home.elements.edges.filter((n) => !!!n.node.adEvents);
     $done({ body: JSON.stringify(body) });
   } else if (body.data.home) {
     body.data.home.elements.edges = body.data.home.elements.edges.filter((n) => !!!n.node.Promoted);
