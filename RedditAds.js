aabdee5c1654628d1fd80f0fd048afd3619e31e9
StricklandF <83151298+StricklandF@users.noreply.github.com>
2022-12-16 22:34:11 +0800

    Update RedditAds.js

--- RedditAds.js
+++ RedditAds.js
@@ -1,10 +1,16 @@
-if ($response.body.includes("Promoted")) {
+if ($response.body.includes("adEvents" | "promoted")) {
   let body = JSON.parse($response.body);
   if (body.data.subredditInfoByName) {
-    body.data.subredditInfoByName.elements.edges = body.data.subredditInfoByName.elements.edges.filter((n) => !!!n.node.Promoted);
+    body.data.subredditInfoByName.elements.edges = body.data.subredditInfoByName.elements.edges.filter((n) => !!!n.node.adEvents);
+    $done({ body: JSON.stringify(body) });
+  } else if (body.data.subredditInfoByName) {
+    body.data.subredditInfoByName.elements.edges = body.data.subredditInfoByName.elements.edges.filter((n) => !!!n.node.promoted);
     $done({ body: JSON.stringify(body) });
   } else if (body.data.home) {
-    body.data.home.elements.edges = body.data.home.elements.edges.filter((n) => !!!n.node.Promoted);
+    body.data.home.elements.edges = body.data.home.elements.edges.filter((n) => !!!n.node.adEvents);
+    $done({ body: JSON.stringify(body) });
+  } else if (body.data.home) {
+    body.data.home.elements.edges = body.data.home.elements.edges.filter((n) => !!!n.node.promoted);
     $done({ body: JSON.stringify(body) });
   } else {
     $done({});
